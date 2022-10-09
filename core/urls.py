4a5e40d9a8a56883c3510409bc094b371729e3e1
Ibtsam Ahmad <ibtsam.ahmad@maqsoodlabs.com>
2022-10-10 01:03:47 +0500

    csrf exempy

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-
+from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
-    path("", views.homepage, name="generate-bill"),
-    path("download-pdf/<year>/<month>", views.generate_PDF, name="download-pdf"),
+    path("", csrf_exempt(views.homepage), name="generate-bill"),
+    path("download-pdf/<year>/<month>", csrf_exempt(views.generate_PDF), name="download-pdf"),
  
 ]
--- kapp/settings.py
+++ kapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$pq!e^)i0w+0w=4(7we5u&yue@!u+6wp-#c^2mwi&=z4icho7i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ibtsam3301.pythonanywhere.com']
+ALLOWED_HOSTS = ['ibtsam3301.pythonanywhere.com', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['http://*', 'https://*']
 ALLOWED_ORIGINS = ['http://*', 'https://*']
 
