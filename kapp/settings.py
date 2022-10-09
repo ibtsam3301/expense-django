0baf1434a8452ca345e732caa9194373bdfba413
Ibtsam Ahmad <ibtsam.ahmad@maqsoodlabs.com>
2022-10-10 01:48:20 +0500

    csrf

--- kapp/settings.py
+++ kapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$pq!e^)i0w+0w=4(7we5u&yue@!u+6wp-#c^2mwi&=z4icho7i
 DEBUG = True
 
 ALLOWED_HOSTS = ['ibtsam3301.pythonanywhere.com', '127.0.0.1']
-ALLOWED_ORIGINS = ['http://*.ibtsam3301.pythonanywhere.com', 'https://*.ibtsam3301.pythonanywhere.com']
+ALLOWED_ORIGINS = ['http://ibtsam3301.pythonanywhere.com', 'https://ibtsam3301.pythonanywhere.com']
 CSRF_TRUSTED_ORIGINS = ALLOWED_ORIGINS.copy()
 CSRF_COOKIE_SECURE = False
 SESSION_COOKIE_SECURE = False
