3e28e8a512071a886a073d9a72f6965ea5ada596
Ibtsam Ahmad <ibtsam.ahmad@maqsoodlabs.com>
2022-10-09 23:11:48 +0500

    debug

--- kapp/settings.py
+++ kapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$pq!e^)i0w+0w=4(7we5u&yue@!u+6wp-#c^2mwi&=z4icho7i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ibtsam3301.pythonanywhere.com']
 CSRF_TRUSTED_ORIGINS = ['http://*', 'https://*']
